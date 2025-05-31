aff4573c322002202cec785ace3d3f2dd154e7ca
TasneemBinti <bintitasneem.ft@bau.edu.bd>
2025-05-31 10:45:49 +0600

    basic2

---
+++ D-3/Basic_part1.R
@@ -0,0 +1,9 @@
+#counting
+
+my.height=65
+my.age=28
+my.weight=50
+x=(my.age*my.height)/my.weight
+print(x)
+students_height=c(34, 43, 35)
+mean(students_height)
---
+++ D-4/basics_part2.R
@@ -0,0 +1,27 @@
+a=11
+b=7.9
+class(a)
+name= 'binti'
+class(name)
+c='5678'
+print(c+76)
+
+#logical data type-True,false(T,F)
+x=19
+y=22-3
+4==4&&5==5
+x!=y||x==y
+
+# simple mathematical operator
+
+z=6+9-8*2/3
+print(z)
+y=6+9-8*(3/3)
+print(y)
+w=4+3-3*2/3
+11%%5
+11%/%5
+
+
+
+

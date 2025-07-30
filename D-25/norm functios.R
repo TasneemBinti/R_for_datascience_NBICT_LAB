2a275ae85c5715404e44fdcaffdb7667a1813fb4
TasneemBinti <bintitasneem.ft@bau.edu.bd>
2025-07-30 22:30:14 +0600

    plot for n dis.

---
+++ D-25/norm functios.R
@@ -0,0 +1,33 @@
+#rnom- random normal
+rnorm(10)
+x=rnorm(10)
+mean(x)
+sd(x)
+y=rnorm(10000)
+mean(y)
+sd(y)
+z=rnorm(10,mean=100,sd=5)
+z
+mean(z)
+sd(z)
+
+#pnorm
+p value calculate korar jnno use hoy
+#At z=0, then the value of probality 0.5
+pnorm(0)
+pnorm(-3)
+pnorm(0.12)
+pnorm(-2)
+#qnorm
+qnorm(0.5)
+qnorm(0.05)
+
+#dnorm
+Y axis value for z=0
+
+
+
+
+
+
+
---
+++ D-26/norm distr. visualization.R
@@ -0,0 +1,30 @@
+#installing the visualize package
+install.packages('visualize')
+library("visualize")
+
+#visualize normal distrubution
+pnorm(-1.96)
+visualize.norm(-1.96)
+visualize.norm(1.96)
+
+#right tail highlight
+visualize.norm(1.96, section="upper")
+
+visualize.norm(stat=c(-1.96,1.96),section="bounded")
+visualize.norm(stat=c(-1.96,1.96),section="tails")
+visualize.norm(stat=c(-1,1),section="bounded")
+visualize.norm(stat=c(-3,3),section="bounded")
+visualize.norm(stat=c(-3,3),section="tails")
+
+#right tail probability
+1-pnorm(2)
+
+visualize.norm(stat=77,mu=75,sd=1,section="lower")
+
+#pnorm (z value)
+
+pnorm(77,mean=75,sd=1)...........left tail
+1-pnorm(77,mean=75,sd=1)............right tail
+
+
+

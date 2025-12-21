c840390ae5f3c89fe1af82484bbd47523a23b80d
TasneemBinti <bintitasneem.ft@bau.edu.bd>
2025-12-21 19:01:32 +0600

    all

---
+++ D-54/SVR-2.R
@@ -0,0 +1,48 @@
+#Setup: packages and data loading
+#install.packages(c("e1071","ggplot2","dplyr"))
+library(e1071)
+library(ggplot2)
+library(dplyr)
+ 
+#loading the dataset
+dataset=read.csv("day.csv")
+
+
+#Basic cleaning and feature selection
+bike=dataset%>%
+  select(cnt,temp,atemp,hum,windspeed,season,yr,mnth,holiday,weekday,workingday,weathersit)
+#convert categorical variables to factors
+bike=bike%>% mutate(season=factor(season),yr=factor(yr),mnth=factor(mnth),holiday=factor(holiday),
+                    weekday=factor(weekday),workingday=factor(workingday),weathersit=factor(weathersit))
+str(bike)
+
+#Training/Test Split
+
+sat.seed(123)
+n=nrow(bike)
+indices= seq_len(n)
+train_idx=sample(indices,size=0.7*n)
+
+
+train_set=bike[train_idx, ]
+train_set
+test_set=bike[-train_idx, ]
+test_set
+
+#Scale numeric features(feature scaling)
+#identify numeric predictors
+num_cols=c("temp","atemp","hum","windspeed")
+
+#Cummute scaling parameters on training data
+train_means=sapply(train_set[, num_cols],mean)
+train_sds=sapply(train_set[, num_cols],sd)
+
+#scale Function
+scale_num=function(df){
+  df[, num_cols]=sweep(df[, num_cols],2,train_means,"-")
+  df[, num_cols]=sweep(df[, num_cols],2,train_sds,"/")
+  df
+}
+train_scaled=scale_num(train_set)
+train_scaled
+
